9f2f8cb4ba66bb10a35f485f22b8a6d0f137657f
Andris Kümpel <andrisivankumpel@gmail.com>
2025-04-28 23:32:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("helloworld")
+@app.get("/helloworld")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("funcaoteste")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
